f0b2c4ff80b0b0ae0ecfd55b7543c2869cd63eb6
Mikiejoe <omoshjoe02@gmail.com>
2024-03-31 16:30:01 +0300

    add: Alumni content,components and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
+import AlumniPage from "./Pages/AlumniPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/alumni" element={<AlumniPage />} />
       </Routes>
     </Router>
   );
--- src/Pages/AlumniPage.jsx
+++ src/Pages/AlumniPage.jsx
@@ -1,1 +1,23 @@
-rfce
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import AlumniHero from "../components/Alumni/AlumniHero";
+import FeaturedAlumnis from "../components/Alumni/FeaturedAlumnis";
+import AlumniDirectory from "../components/Alumni/AlumniDirectory";
+import AlumniEvents from "../components/Alumni/AlumniEvents";
+
+function AlumniPage() {
+  return (
+    <>
+      <Header />
+      <AlumniHero />
+      <div className="px-12">
+        <FeaturedAlumnis />
+        <AlumniDirectory />
+        <AlumniEvents />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default AlumniPage;
