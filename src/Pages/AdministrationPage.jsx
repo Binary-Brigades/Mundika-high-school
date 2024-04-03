df78b2ba0a269fdb6f486653f4fc093530f69082
Muchael123 <mainamichael2849@gmail.com>
2024-04-03 11:29:42 +0300

    Add(Core): Add administration page content

--- src/Pages/AdministrationPage.jsx
+++ src/Pages/AdministrationPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/common/Header";
+import Sidebar from "../components/common/Sidebar";
+import Footer from "../components/common/Footer";
+import AdminHero from "../components/Administration/AdminHero";
+import AdminText from "../components/Administration/AdminText";
 
 function AdministrationPage() {
-  return <div>AdministrationPage</div>;
+  const [showSideBar, setShowSidebar] = useState(false);
+  return (
+    <div>
+      <Header setShowSidebar={setShowSidebar} />
+      <Sidebar showSideBar={showSideBar} />
+      <AdminHero />
+      <AdminText />
+      <Footer />
+    </div>
+  );
 }
 
 export default AdministrationPage;
