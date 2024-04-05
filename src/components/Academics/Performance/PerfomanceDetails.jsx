e4c3736c1db9f6ee4e0c40d7fc4654339bf543a9
Amianie <amianielvis8@gmail.com>
2024-04-05 19:16:19 +0300

    Performance Page

--- src/components/Academics/Performance/PerfomanceDetails.jsx
+++ src/components/Academics/Performance/PerfomanceDetails.jsx
@@ -1,84 +1,153 @@
-import React from 'react'
+import React from "react";
 
 function PerfomanceDetails() {
   return (
     <div className="w-screen md:text-sm  justify-start px-[8%] md:px-14 lg:px-20 flex flex-col">
-        <p>School Perfomance</p>
-        <table className="table p-4 bg-[#b08b8d]rounded-lg shadow">
-          <thead>
-              <tr>
-                  <th className=" text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      #
-                  </th>
-                  <th className=" text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      Year
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      A
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      A-
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      B+
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      B
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      B-
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      C+
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      C
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      C-
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      D+
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      D
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      D-
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal textgit-gray-900">
-                      ENTRY
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
-                      MEAN
-                  </th>
-                  <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
-                      MEAN GRADE
-                  </th>
-              </tr>
-          </thead>
-          <tbody>
-            <tr className="text-gray-700">
-                <td className="font-bold border-b-2 p-4 dark:border-dark-5">
-                    BES
-                </td>
-                <td className="border-b-2 p-4 dark:border-dark-5">
-                    20,200
-                </td>
-                <td className="border-b-2 p-4 dark:border-dark-5">
-                    6,600    
-                </td>
-                <td className="border-b-2 p-4 dark:border-dark-5">
-                    3,585
-                </td>
-                <td className="border-b-2 p-4 dark:border-dark-5">
-                    30,385
-                </td>
-            </tr>
-            </tbody>
-          </table>
+      <p>School Perfomance</p>
+      <table className="table p-4 bg-[#b08b8d]rounded-lg shadow">
+        <thead>
+          <tr>
+            <th className=" text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              #
+            </th>
+            <th className=" text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              Year
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              A
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              A-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              B+
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              B
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              B-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              C+
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              C
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              C-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              D+
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              D
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              D-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold textgit-gray-900">
+              ENTRY
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              MEAN
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              MEAN GRADE
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="text-gray-700">
+            <td className="font-bold border-b-2 p-4 dark:border-dark-5">1</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">2022</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">39</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">120</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">137</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">90</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">54</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">36</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">17</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">7</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">2</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">1</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">0</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">503</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">10</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">B+</td>
+          </tr>
+          <tr className="text-gray-700">
+            <td className="font-bold border-b-2 p-4 dark:border-dark-5">2</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">2021</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">28</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">113</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">113</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">78</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">82</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">47</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">31</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">18</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">11</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">2</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">3</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">526</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">9</td>
+            <td className="border-b-2 p-4 dark:border-dark-5">B</td>
+          </tr>
+           <tr>
+            <th className=" text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              #
+            </th>
+            <th className=" text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              Year
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              A
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              A-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              B+
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              B
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              B-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              C+
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              C
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              C-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              D+
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              D
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              D-
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold textgit-gray-900">
+              ENTRY
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              MEAN
+            </th>
+            <th className="text-left border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-bold text-gray-900">
+              MEAN GRADE
+            </th>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default PerfomanceDetails
+export default PerfomanceDetails;
