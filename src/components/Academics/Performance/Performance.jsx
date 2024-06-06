0ba80df960017a60ca699b880ecac210b01f8eab
Amianie <amianielvis8@gmail.com>
2024-06-06 11:55:31 +0300

    Sidebar

--- src/components/Academics/Performance/Performance.jsx
+++ src/components/Academics/Performance/Performance.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../../common/Header";
 import Footer from "../../common/Footer";
+import Sidebar from "../../../components/common/Sidebar";
 import PerformanceHero from "./PerformanceHero";
 import DeputyPrincipal from "./DeputyPrincipal Details";
 import PerfomanceDetails from "./PerfomanceDetails";
 
 function Performance() {
+  const [showSideBar, setShowSidebar] = useState(false);
   return(
     <>
-    <Header />
+    <Header setShowSidebar={setShowSidebar}/>
+    <Sidebar showSideBar={showSideBar} />
     <PerformanceHero/>
     <DeputyPrincipal/>
     <PerfomanceDetails/>
--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -13,7 +13,7 @@ function About() {
             Sacrifice to Succeed
           </p>
         </div>
-        {/* <div className="bg-[#590000]/30 w-[95%]  flex-col gap-3 rounded-md justify-center items-center flex max-w-[350px] shadow-md md:shadow-lg p-5  border-[1px] border-[#590000]/80">
+        <div className="bg-[#590000]/30 w-[95%]  flex-col gap-3 rounded-md justify-center items-center flex max-w-[350px] shadow-md md:shadow-lg p-5  border-[1px] border-[#590000]/80">
           <h3 className="font-bold capitalize text-[#590000]/80">
             Our Mission
           </h3>
@@ -21,7 +21,7 @@ function About() {
             To empower students with the values of sacrifice, perseverance, and
             excellence to prepare them for academic and personal success.
           </p>
-        </div> */}
+        </div> 
         <div className="bg-[#590000]/30 w-[95%] flex-col gap-3 rounded-md justify-center items-center flex max-w-[350px] shadow-md md:shadow-lg p-5 md:p-8 border-[1px] border-[#590000]/80">
           <h3 className="font-bold capitalize text-[#590000]/80">Our Vision</h3>
           <p className="font-semibold tracking-wide text-[#ffff]/70 text-center">
